915d1c53bc1a487658e33801efffdd84a0026244
Roeland Jago Douma <roeland@famdouma.nl>
2016-11-18 15:20:23 +0100

    Remove files and move config to config.php
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ server/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'settings' => [
+		'displayErrorDetails' => true,
+		'addContentLengthHeader' => true,
+		'db' => [
+			'host' => "172.17.0.2",
+			'user' => "lookup",
+			'pass' => "lookup",
+			'dbname' => "lookup",
+		]
+	]
+];
--- server/index.php
+++ server/index.php
@@ -2,18 +2,7 @@
 
 require 'vendor/autoload.php';
 
-$settings = [
-	'settings' => [
-		'displayErrorDetails' => true,
-		'addContentLengthHeader' => true,
-		'db' => [
-			'host' => "172.17.0.2",
-			'user' => "lookup",
-			'pass' => "lookup",
-			'dbname' => "lookup",
-		]
-	]
-];
+$settings = require('config.php');
 
 $container = new \Slim\Container($settings);
 
