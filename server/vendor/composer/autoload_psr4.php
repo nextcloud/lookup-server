3ab708dd4c6a66936e295a36600d8e2aca2614b7
Roeland Jago Douma <roeland@famdouma.nl>
2016-11-18 09:35:25 +0100

    Add guzzle
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- server/vendor/composer/autoload_psr4.php
+++ server/vendor/composer/autoload_psr4.php
@@ -10,5 +10,8 @@ return array(
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'LookupServer\\' => array($baseDir . '/lib'),
     'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'FastRoute\\' => array($vendorDir . '/nikic/fast-route/src'),
 );
