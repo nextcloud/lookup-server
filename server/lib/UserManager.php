27bce133efd8f391cd04f0165e024c2393f8a005
Micke Nordin <kano@sunet.se>
2021-11-10 11:23:44 +0100

    Lookup server matches email as well as userid
    When there are several users accounts that share the same email address
    AND the userid for one of those accounts is the same as the email
    address, the lookup server will pick a user at random from those
    accounts that share email address, causing the user to be logged in to
    a random server.
    
    This patch will make it so that usernames are only matched against the
    userid, and not the email field.
    
    Fixes: #75
    Signed-off-by: Micke Nordin <kano@sunet.se>

--- server/lib/UserManager.php
+++ server/lib/UserManager.php
@@ -164,7 +164,7 @@ FROM (
 	WHERE userId IN (
 		SELECT DISTINCT userId
 		FROM `store`
-		WHERE v ' . $operator . ' :search ' . $constraint . '
+		WHERE v ' . $operator . ' :search ' . $constraint .' AND k = "userid"
 	)
 	GROUP BY userId
 ) AS tmp
